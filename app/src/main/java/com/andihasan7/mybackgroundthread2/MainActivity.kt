02ef756f03a352885445cf244c7c71606de93267
andihasan97 <andihasanelfalakiy@gmail.com>
2022-10-18 15:13:39 +0700

    test background proses di UIthread/ main thread(freeze)

---
+++ app/src/main/java/com/andihasan7/mybackgroundthread2/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.andihasan7.mybackgroundthread2
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnStart = findViewById<Button>(R.id.btn_start)
+        val tvStatus = findViewById<TextView>(R.id.tv_status)
+
+        btnStart.setOnClickListener {
+            try {
+                for (i in 0..10) {
+                    Thread.sleep(500) // jika terlalu lama aplikasi akan force close
+                    val percentage = i * 10
+                    if (percentage == 100) {
+                        tvStatus.setText(R.string.task_completed)
+                    } else {
+                        tvStatus.text = String.format(getString(R.string.compressing), percentage)
+                    }
+                }
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
